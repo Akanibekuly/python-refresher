ecc67172a4b1417339c6780348491d5ee220044b
Akanibekuly <a.kanibekuly@gmail.com>
2020-10-30 18:39:11 +0600

    feat: end of tutorial

--- python_udemy/34_simple_decorators/code.py
+++ python_udemy/34_simple_decorators/code.py
@@ -1,11 +1,16 @@
-user={"username": "jose", "access_level": "admin"}
+user={"username": "jose", "access_level": "quest"}
 
 def get_admin_password():
     return "1234"
 
-def secure_function(func):
-    if user["access_level"]=="admin":
-        return func
+def make_secure(func):
+    def secure_function():
+        if user["access_level"]=="admin":
+            return func()
+        else: 
+            return f"No admin permissions for {user['username']}."
+    
+    return secure_function
 
-get_admin_password=secure_function(get_admin_password)
+get_admin_password=make_secure(get_admin_password)
 print(get_admin_password())
